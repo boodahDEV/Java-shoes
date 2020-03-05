540db6725a3a217749d9a641811b37c50c4ed2f0
boodah <boodah21@protonmail.com>
2020-03-05 10:45:26 -0500

    Se prepara el paquete para contener la GUI de la app.

--- GitComandsUI/src/main/Main.java
+++ GitComandsUI/src/main/Main.java
@@ -3,6 +3,9 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
+		/**
+		 * TESTS DE EXTRACCIONES DE COMANDOS, PROVADO TEMPORALMENTE EN WINDOWS 10
+		 * */
 		 User u = new User();
 			System.out.println( u.getUserName("git config user.name"));
 			System.out.println(  u.getUserEmail("git config user.email"));
