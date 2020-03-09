f984582e856bb987d217595c36953bf06cfa36a3
boodah <boodah21@protonmail.com>
2020-03-09 17:30:24 -0500

    Se inicia desarrollo de la interfaz grafica.

--- GitComandsUI/src/GUI/Interfaz_git.java
+++ GitComandsUI/src/GUI/Interfaz_git.java
@@ -1,42 +1,54 @@
 package GUI;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Color;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
 
 public class Interfaz_git extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Interfaz_git frame = new Interfaz_git();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		Interfaz_git frame = new Interfaz_git();	
+		frame.loadPanelsInContentPane();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);	
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public Interfaz_git() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setSize(450, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setBorder(new MatteBorder(2,2,2,2, new Color(213,0,249)));
+		contentPane.setLayout(null);
+		
+		/*	CONFIGURACION INICIAL */
+		
 		setContentPane(contentPane);
+		this.setUndecorated(true);
+		setResizable(false);
+	}
+	
+	public void loadPanelsInContentPane() {
+		JPanel [] allPane = new JPanel[1]; // instancia de paneles
+		
+		/* 0 -> Panel norte "barra de titulo"  */
+		allPane[0] = new JPanel(null);
+		allPane[0].setBounds(1, 1, 448, 30);
+		allPane[0].setBackground(new Color(170,0,255));
+		/* 0 -> Panel norte "barra de titulo"  */
+
+		
+		
+		for (JPanel jPanel : allPane) {
+			contentPane.add(jPanel);			
+		}
 	}
 
 }
